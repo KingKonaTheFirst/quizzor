f081ac93b7db6d012c727a7b99b53d0283e0f6b6
Kona <landon.deroin@gmail.com>
2023-09-01 10:40:01 -0400

    added some goodies on the timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,69 @@ let currentQuestion = 0;
 let score = 0;
 let timer = 60; // Timer in seconds
 
-
 const questions = [
   {
     question: "What does JS stand for in JavaScript?",
     options: ["JavaSource", "JustScript", "JumboScript", "JavaScript"],
-    answer: 3
-}
-]
+    answer: 3,
+  },
+  {
+    question: "Which of the following is a valid JavaScript data type?",
+    options: ["Character", "String", "Number", "All of the above"],
+    answer: 1,
+  },
+
+  {
+    question: "How can you write a comment in JavaScript?",
+    options: [
+      "//This is a comment",
+      "<!--This is a comment-->",
+      "/*This is a comment*/",
+      "Both 1 and 3",
+    ],
+    answer: 3,
+  },
+
+  {
+    question: "What is the correct way to create an array in JavaScript?",
+    options: [
+      'var colors = ("red", "green", "blue")',
+      'var colors = ["red", "green", "blue"]',
+      'var colors = "red", "green", "blue"',
+      'var colors = array("red", "green", "blue")',
+    ],
+    answer: 1,
+  },
+
+  {
+    question: "Which of the following is not a valid JavaScript operator?",
+    options: ["+", "&", "%", "/"],
+    answer: 1,
+  },
+
+  {
+    question: "How do you declare a variable in JavaScript?",
+    options: [
+      "var myVariable;",
+      "myVariable;",
+      "variable myVariable;",
+      "declare myVariable;",
+    ],
+    answer: 0,
+  },
+
+  {
+    question:
+      "Which of the following will write 'Hello World' in an alert box?",
+    options: [
+      "alertBox('Hello World')",
+      "msgAlert('Hello World')",
+      "msg('Hello World')",
+      "alert('Hello World')",
+    ],
+    answer: 3,
+  },
+];
 
 const questionElement = document.getElementById("question");
 const optionsElement = document.getElementById("options");
@@ -19,21 +74,18 @@ const scoreElement = document.getElementById("score");
 
 function displayQuestion() {
   if (currentQuestion < questions.length) {
-    
     questionElement.textContent = questions[currentQuestion].question;
-    
+
     optionsElement.innerHTML = "";
-    
+
     questions[currentQuestion].options.forEach((option, index) => {
-      
-        const optionElement = document.createElement("div");
-      
-        optionElement.textContent = option;
-      
-        optionElement.addEventListener("click", () => checkAnswer(index));
-      
-        optionsElement.appendChild(optionElement);
-    
+      const optionElement = document.createElement("div");
+
+      optionElement.textContent = option;
+
+      optionElement.addEventListener("click", () => checkAnswer(index));
+
+      optionsElement.appendChild(optionElement);
     });
   } else {
     displayResults();
@@ -43,6 +95,8 @@ function displayQuestion() {
 function checkAnswer(selectedOption) {
   if (selectedOption === questions[currentQuestion].answer) {
     score++;
+  } else {
+    timer ;
   }
   currentQuestion++;
   displayQuestion();
@@ -54,6 +108,10 @@ function displayResults() {
   submitButton.style.display = "none";
   resultsElement.style.display = "block";
   scoreElement.textContent = `Your score: ${score} out of ${questions.length}`;
+
+  const initials = prompt("Please enter your initials:");
+  localStorage.setItem("initials", initials);
+  localStorage.setItem("score", score);
 }
 
 function updateTimer() {
